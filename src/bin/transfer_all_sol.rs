7dfebd363f00f0c18714433a4ade283d49927bef
Max Block <maxblock@pm.me>
2022-04-12 09:19:51 +0300

    update

--- src/bin/transfer_all_sol.rs
+++ src/bin/transfer_all_sol.rs
@@ -3,7 +3,7 @@ use std::str::FromStr;
 use solana_client::rpc_client::RpcClient;
 use solana_sdk::native_token::sol_to_lamports;
 use solana_sdk::pubkey::Pubkey;
-use solana_sdk::signature::Keypair;
+use solana_sdk::signature::{Keypair, Signer};
 use solana_sdk::system_transaction;
 
 use learn::Env;
@@ -11,9 +11,11 @@ use learn::Env;
 fn main() {
     let env = Env::new();
     let client = RpcClient::new(env.rpc_url);
-    let from_keypair = Keypair::from_base58_string(&env.private_key_0);
-    let recipient = Pubkey::from_str(&env.address_1).unwrap();
-    let lamports = sol_to_lamports(0.12);
+    let from_keypair = Keypair::from_base58_string(&env.private_key_1);
+    let recipient = Pubkey::from_str(&env.address_0).unwrap();
+
+    let balance = client.get_balance(&from_keypair.pubkey()).unwrap();
+    let lamports = balance - 5000;
     let recent_blockhash = client.get_latest_blockhash().unwrap();
     dbg!(recent_blockhash);
     let tx = system_transaction::transfer(&from_keypair, &recipient, lamports, recent_blockhash);
